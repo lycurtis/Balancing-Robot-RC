b2bf000ac8a13f5c8c906b4130224e65b9cf5df5
lycurtis <137116222+lycurtis@users.noreply.github.com>
2024-06-03 23:49:03 -0700

    Update controllerR.ino

--- Code/controllerR.ino
+++ Code/controllerR.ino
@@ -1,215 +1,392 @@
-#include <RF24.h>
-#include <RF24_config.h>
-#include <nRF24L01.h>
-#include <printf.h>
-#include <SPI.h>
+#include "Timer.h"
 
-#define CE_PIN 9 // teensy 4.1
-#define CSN_PIN 10 // teensy 4.1
+#define TOTAL_TASK 2
+#define PERIOD_GCD 10
+#define PERIOD_FBCONTROL 10
+#define PERIOD_RCONTROL 10
+#define led1 5
+#define led2 6
+#define led3 4
+#define led4 3
 
-RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
+#define led5 2 //right 
+#define led6 9 //mostright
+#define led7 10 //left
 
-const byte address[6] = "00001";
+// Define the analog pins for the joystick
+const int joystickLPin = A0;
+const int joystickRPin = A1;
 
-const int forwardLED1 = 32;
-const int forwardLED2 = 31;
-const int backwardLED1 = 30;
-const int backwardLED2 = 29;
-const int leftLED = 40;
-const int rightLED = 41;
+const int fullThrottleTH = 1022;
+const int fullReverseTH = 1;
 
-IntervalTimer pitchTimer;
-IntervalTimer yawTimer; //rotation timer
+int joyL;
+int joyR;
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting receiver setup");
 
-  pitchTimer.begin(joyX, 100000);
-  yawTimer.begin(joyY, 100000);
+typedef struct task{
+  unsigned short period;
+  unsigned short timeElapsed;
 
-  Serial.println("Initializing radio...");
-  if (radio.begin()) {
-    Serial.println("Radio initialized successfully");
-    radio.printDetails(); // Print details for debugging
-  } else {
-    Serial.println("Radio initialization failed");
-    while (true); // Halt execution if initialization fails
+  void(*tick) (void);
+};
+
+static struct task gTaskSet[TOTAL_TASK];
+
+void initializeTask(void){
+  gTaskSet[0].period = PERIOD_FBCONTROL;
+  gTaskSet[0].timeElapsed = 0;
+  gTaskSet[0].tick = tickFB;
+  
+  
+  gTaskSet[1].period = PERIOD_RCONTROL;
+  gTaskSet[1].timeElapsed = 0;
+  gTaskSet[1].tick = tickR;
+  
+  
+}
+
+void scheduleTask(){
+  for(int i = 0; i < TOTAL_TASK; i++){
+    gTaskSet[i].timeElapsed += PERIOD_GCD;
+    if(gTaskSet[i].timeElapsed >= gTaskSet[i].period){
+      gTaskSet[i].tick();
+      gTaskSet[i].timeElapsed = 0;
+    }
+  }
+}
+
+enum STATES_FBCONTROL {INIT, READ, UP, UPMAX, DOWN, DOWNMAX} lState = INIT;
+enum STATES_RCONTROL {INIT1, READ1, RIGHT, RIGHTMAX, LEFT, LEFTMAX} rState = INIT1;
+
+void tickFB(void){ //528 rest value (0 down, 1023 up)
+  switch(lState){
+    case INIT:
+      lState = READ;
+    break;
+
+    case READ:
+      if(joyL > 529 && joyL < fullThrottleTH){
+        lState = UP;
+      }
+      else if(joyL >= fullThrottleTH){
+        lState = UPMAX;
+      }
+      else if(joyL < 527 && joyL > fullReverseTH){
+        lState = DOWN;
+      }
+      else if(joyL <= fullReverseTH){
+        lState = DOWNMAX;
+      }
+      else if (joyL >= 527 && joyL <=529){
+        lState = READ;
+      }
+    break;
+
+    case UP:
+      if(joyL > 529 && joyL < fullThrottleTH){
+        lState = UP;
+      }
+      else if(joyL >= fullThrottleTH){
+        lState = UPMAX;
+      }
+      else if(joyL < 527 && joyL > fullReverseTH){
+        lState = DOWN;
+      }
+      else if(joyL <= fullReverseTH){
+        lState = DOWNMAX;
+      }
+      else if (joyL >= 527 && joyL <=529){
+        lState = READ;
+      }
+    break;
+
+    case UPMAX:
+      if(joyL > 529 && joyL < fullThrottleTH){
+        lState = UP;
+      }
+      else if(joyL >= fullThrottleTH){
+        lState = UPMAX;
+      }
+      else if(joyL < 527 && joyL > fullReverseTH){
+        lState = DOWN;
+      }
+      else if(joyL <= fullReverseTH){
+        lState = DOWNMAX;
+      }
+      else if (joyL >= 527 && joyL <=529){
+        lState = READ;
+      }
+    break;
+
+    case DOWN:
+      if(joyL > 529 && joyL < fullThrottleTH){
+        lState = UP;
+      }
+      else if(joyL >= fullThrottleTH){
+        lState = UPMAX;
+      }
+      else if(joyL < 527 && joyL > fullReverseTH){
+        lState = DOWN;
+      }
+      else if(joyL <= fullReverseTH){
+        lState = DOWNMAX;
+      }
+      else if (joyL >= 527 && joyL <=529){
+        lState = READ;
+      }
+    break;
+
+    case DOWNMAX:
+      if(joyL > 529 && joyL < fullThrottleTH){
+        lState = UP;
+      }
+      else if(joyL >= fullThrottleTH){
+        lState = UPMAX;
+      }
+      else if(joyL < 527 && joyL > fullReverseTH){
+        lState = DOWN;
+      }
+      else if(joyL <= fullReverseTH){
+        lState = DOWNMAX;
+      }
+      else if (joyL >= 527 && joyL <=529){
+        lState = READ;
+      }
+    break;
+
+    default:
+      lState = READ;
+    break;
   }
 
-  radio.setChannel(90); // Change channel to avoid interference
-  radio.setPALevel(RF24_PA_LOW); // Set power level to minimum
-  radio.openReadingPipe(0, address);
-  radio.startListening();
-  Serial.println("Receiver setup done");
+  //actions
+  switch(lState){
+    case INIT:
+    break;
 
-  pinMode(forwardLED1, OUTPUT);
-  pinMode(forwardLED2, OUTPUT);
-  pinMode(backwardLED1, OUTPUT);
-  pinMode(backwardLED2, OUTPUT);
-  pinMode(leftLED, OUTPUT);
-  pinMode(rightLED, OUTPUT);
-
-  digitalWrite(forwardLED1, LOW);
-  digitalWrite(forwardLED2, LOW);
-  digitalWrite(backwardLED1, LOW);
-  digitalWrite(backwardLED2, LOW);
-  digitalWrite(leftLED, LOW);
-  digitalWrite(rightLED, LOW);
-}
-
-enum STATES_joyY {IDLEY, FORWARD_MIN, FORWARD_MAX, BACKWARD_MIN, BACKWARD_MAX} jState = IDLEY;
-enum STATES_joyX {IDLEX, LEFT_LED, RIGHT_LED} gState = IDLEX;
-
-struct JoystickData {
-  int forwardBackward;
-  int leftRight;
-}data;
-    
-
-void joyY(){
-    radio.read(&data, sizeof(data));
-
-    int fbValue = map(data.forwardBackward, 0, 1023, 0, 100);
-
-    // Thresholds for joystick detection
-    int thresholdUp = 60;
-    int thresholdDown = 40;
-    int secondLEDThresholdUp = 98; 
-    int secondLEDThresholdDown = 2;
-
-    // Forward/Backward LEDs
-    //transitions
-    switch (jState) {
-      case IDLEY: //No actuation
-        if (fbValue > thresholdUp) {
-          jState = FORWARD_MIN;
-        }
-        else if (fbValue < thresholdDown) {
-          jState = BACKWARD_MIN;
-        }
-        break;
-
-      case FORWARD_MIN:
-        if (fbValue > secondLEDThresholdUp) {
-          jState = FORWARD_MAX;
-        }
-        else if (fbValue <= thresholdDown) {
-          jState = IDLEY;
-        }
-        break;
-
-      case FORWARD_MAX:
-        if (fbValue <= secondLEDThresholdUp && fbValue > thresholdDown) {
-          jState = FORWARD_MIN;
-        }
-        else if (fbValue <= thresholdUp) {
-          jState = IDLEY;
-        }
-        break;
+    case READ:
       
-      case BACKWARD_MIN:
-        if (fbValue <= secondLEDThresholdDown) {
-          jState = BACKWARD_MAX;
-        }
-        else if (fbValue >= thresholdUp) {
-          jState = IDLEY;
-        }
-        break;
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+    break;
 
-      case BACKWARD_MAX:
-        if (fbValue > secondLEDThresholdDown) {
-          jState = BACKWARD_MIN;
-        }
-        else if (fbValue > thresholdDown) {
-          jState = IDLEY;
-        }
-        break;
-      default:
-        jState = IDLEY;
-        break;
-    }
-    //actions
-    switch(gState) {
-      case IDLEY:
-        digitalWrite (forwardLED1, LOW);
-        digitalWrite (forwardLED2, LOW);
-        digitalWrite (backwardLED1, LOW);
-        digitalWrite (backwardLED2, LOW);
-        break;
-      case FORWARD_MIN:
-        digitalWrite (forwardLED1, HIGH);
-        digitalWrite (forwardLED2, LOW);
-        digitalWrite (backwardLED1, LOW);
-        digitalWrite (backwardLED2, LOW);
-        break;
-      case FORWARD_MAX:
-        digitalWrite (forwardLED1, HIGH);
-        digitalWrite (forwardLED2, HIGH);
-        digitalWrite (backwardLED1, LOW);
-        digitalWrite (backwardLED2, LOW);
-        break;
-      case BACKWARD_MIN: 
-        digitalWrite (forwardLED1, LOW);
-        digitalWrite (forwardLED2, LOW);
-        digitalWrite (backwardLED1, HIGH);
-        digitalWrite (backwardLED2, LOW);
-        break;
-      case BACKWARD_MAX: 
-        digitalWrite (forwardLED1, LOW);
-        digitalWrite (forwardLED2, LOW);
-        digitalWrite (backwardLED1, HIGH);
-        digitalWrite (backwardLED2, HIGH);
-        break;
-    }
+    case UP:
+      
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      
+
+    break;
+
+    case UPMAX:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+    break;
+
+    case DOWN:
+      
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, LOW);
+      
+    break;
+
+    case DOWNMAX:
+      
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+
+    break;
+  }
 }
 
+void tickR(void){ //501 rest val (left 0, right 1023)
+  switch(rState){
+    case INIT1:
+      rState = READ1;
+    break;
 
-void joyX(){
-    radio.read(&data, sizeof(data));
+    case READ1:
+      if(joyR > 502 && joyR < fullThrottleTH){
+        rState = RIGHT;
+      }
+      else if(joyR >= fullThrottleTH){
+        rState = RIGHTMAX;
+      }
+      else if(joyR < 500 && joyR > fullReverseTH){
+        rState = LEFT;
+      }
+      else if(joyR <= fullReverseTH){
+        rState = LEFTMAX;
+      }
+      else if (joyR >= 500 && joyR <=502){
+        rState = READ1;
+      }
+    break;
 
-    int lrValue = map(data.leftRight, 0, 1023, 0, 100);
+    case RIGHT:
+      if(joyR > 502 && joyR < fullThrottleTH){
+        rState = RIGHT;
+      }
+      else if(joyR >= fullThrottleTH){
+        rState = RIGHTMAX;
+      }
+      else if(joyR < 500 && joyR > fullReverseTH){
+        rState = LEFT;
+      }
+      else if(joyR <= fullReverseTH){
+        rState = LEFTMAX;
+      }
+      else if (joyR >= 500 && joyR <=502){
+        rState = READ1;
+      }
+    break;
 
-    //transitions
-    switch(gState) {
-      case IDLEX:
-        if (lrValue >= 49 && lrValue <= 51) {
-          gState = IDLEX;
-        }
-        else if(lrValue > 51){
-          gState = RIGHT_LED:
-        }
-        else if (lrValue < 49) {
-          gState = LEFT_LED;
-        }
-        break;
-      case LEFT_LED:
-        if (lrValue <= 51) {
-          gState = LEFT_LED;
-        }
-        else {
-          gState = IDLEX;
-        }
-        break;
-      case RIGHT_LED:
-        if (lrValue >= 51) {
-          gState = RIGHT_LED;
-        }
-        else {
-          gState = IDLEX;
-        }
-        break;
-    }
-    //actions
-    switch(gState) {
-      case LEFT_LED:
-        digitalWrite(leftLED, HIGH);
-        digitalWrite(rightLED, LOW);
-      case RIGHT_LED:
-        digitalWrite(rightLED, HIGH);
-        digitalWrite(leftLED, LOW);
-    }
+    case RIGHTMAX:
+      if(joyR > 502 && joyR < fullThrottleTH){
+        rState = RIGHT;
+      }
+      else if(joyR >= fullThrottleTH){
+        rState = RIGHTMAX;
+      }
+      else if(joyR < 500 && joyR > fullReverseTH){
+        rState = LEFT;
+      }
+      else if(joyR <= fullReverseTH){
+        rState = LEFTMAX;
+      }
+      else if (joyR >= 500 && joyR <=502){
+        rState = READ1;
+      }
+    break;
+
+    case LEFT:
+      if(joyR > 502 && joyR < fullThrottleTH){
+        rState = RIGHT;
+      }
+      else if(joyR >= fullThrottleTH){
+        rState = RIGHTMAX;
+      }
+      else if(joyR < 500 && joyR > fullReverseTH){
+        rState = LEFT;
+      }
+      else if(joyR <= fullReverseTH){
+        rState = LEFTMAX;
+      }
+      else if (joyR >= 500 && joyR <=502){
+        rState = READ1;
+      }
+    break;
+
+    case LEFTMAX:
+      if(joyR > 502 && joyR < fullThrottleTH){
+        rState = RIGHT;
+      }
+      else if(joyR >= fullThrottleTH){
+        rState = RIGHTMAX;
+      }
+      else if(joyR < 500 && joyR > fullReverseTH){
+        rState = LEFT;
+      }
+      else if(joyR <= fullReverseTH){
+        rState = LEFTMAX;
+      }
+      else if (joyR >= 500 && joyR <=502){
+        rState = READ1;
+      }
+    break;
+
+    default:
+      rState = READ1;
+    break;
+  }
+
+  //actions
+  switch(rState){
+    case INIT1:
+    break;
+
+    case READ1:
+      
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+      digitalWrite(A4, LOW);
+    break;
+
+    case RIGHT:
+      
+      digitalWrite(led5, HIGH);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+      digitalWrite(A4, LOW);
+      
+
+    break;
+
+    case RIGHTMAX:
+      digitalWrite(led5, HIGH);
+      digitalWrite(led6, HIGH);
+      digitalWrite(led7, LOW);
+      digitalWrite(A4, LOW);
+    break;
+
+    case LEFT:
+      
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, HIGH);
+      digitalWrite(A4, LOW);
+      
+    break;
+
+    case LEFTMAX:
+      
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, HIGH);
+      digitalWrite(A4, HIGH);
+
+    break;
+  }
+}
+
+void setup() {
+  // Initialize serial communication at 9600 bits per second
+  Serial.begin(9600);
+  TimerSet(PERIOD_GCD);
+  TimerOn();
+  initializeTask();
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT); //left
+  pinMode(A4, OUTPUT); //most left
 }
 
 void loop() {
+  // Read the raw values from the joystick
+  joyL = analogRead(joystickLPin);
+  joyR = analogRead(joystickRPin);
+
+  // Print the raw values to the Serial Monitor
+
+  scheduleTask();
+  while(!TimerFlag){}
+  TimerFlag = 0;
 }
