96d90059cd9fb10f1c551dd84290d9f5e8641dfd
lycurtis <137116222+lycurtis@users.noreply.github.com>
2024-06-05 05:33:49 -0700

    Create readfromkin.ino

---
+++ Code/readfromkin.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+char buff [32];
+volatile byte indx;
+volatile boolean process;
+ 
+void setup (void) {
+   Serial.begin (115200);
+   pinMode(MISO, OUTPUT); // have to send on master in so it set as output
+   SPCR |= _BV(SPE); // turn on SPI in slave mode
+   indx = 0; // buffer empty
+   process = false;
+   SPI.attachInterrupt(); // turn on interrupt
+}
+ 
+ISR (SPI_STC_vect) // SPI interrupt routine 
+{ 
+   byte c = SPDR; // read byte from SPI Data Register
+
+   if (indx < sizeof(buff)) {
+      buff[indx++] = c; // save data in the next index in the array buff
+      if (c == '\n') { 
+        buff[indx - 1] = 0; // replace newline ('\n') with end of string (0)
+        process = true;
+      }
+   }
+}
+ 
+void loop (void) {
+   if (process) {
+      process = false; //reset the process
+      Serial.println (buff); //print the array on serial monitor
+      indx= 0; //reset button to zero
+   }
+}
